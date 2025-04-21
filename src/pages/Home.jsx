723cc09b0a5c38b6bd8814a65ae248accaf32162
Ian Childress <howie2077@yahoo.com>
2025-04-20 21:36:35 -0400

    added a gradient color scheme to the page, looking to make images coalesce better with the background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,18 @@ import NavBar from "../Components/NavBar.jsx";
 function Home() {
   return (
     <>
-    <div className="nav">
-      <NavBar></NavBar>
-    </div>
+      <div className="nav">
+        <NavBar></NavBar>
+      </div>
       <div className="home">
         <HomeContent></HomeContent>
-        <p>We specialize in custom brick and stone work</p>
+        <div className="h2">
+          <h2>Brick, Block, Or Stone, We Can Do It All!</h2>
+          <p className="h2p">
+            With 58+ years of business we ensure all of your masonry needs are
+            met with our highest quality craftsman-ship.
+          </p>
+        </div>
       </div>
     </>
   );
