e5bf7ae6921a19852ae54ef0bdb1408bca8ac1ae
Ian Childress <howie2077@yahoo.com>
2025-08-11 19:59:01 -0400

    navbar media queries complete

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,30 +4,36 @@ import "../App.css";
 import logo from "../images/project1.jpg";
 
 const NavBar = () => {
-    /* this is the useState hook its how you check the state of a variable or object.*/
-    const [dropDownVisible, setDropDownVisible] = React.useState(false);
   return (
     <nav className="navbar">
       <a href="/">
         <img src={logo} className="logo"></img>
       </a>
-      <ul className="nav-links">
-        <li className="dropdown" onMouseEnter={() => setDropDownVisible(true)} onMouseLeave={() => setDropDownVisible(false)}>
-  
-            {/* CONDITIONAL RENDER basically an 'if' statement*/}
-           
-            <ul className="dropdown-menu">
-              <li><Link to="/" id="linkTo">Home</Link></li>
-              <li><Link to="/projects" id="linkTo">Services</Link></li>
-              <li><Link to="/about" id="linkTo">About</Link></li>
-              <li><Link to="/contact" id="linkTo">Contact</Link></li>
-            </ul>
-        
+
+      <ul className="dropdown-menu">
+        <li>
+          <Link to="/" className="linkTo">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/projects" className="linkTo">
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="linkTo">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="linkTo">
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
   );
 };
-
 
 export default NavBar;
