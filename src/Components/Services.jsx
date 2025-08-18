feb434479575975511a2c731c337f5f020a9ee15
Ian Childress <howie2077@yahoo.com>
2025-08-17 20:32:00 -0400

    testing aos on mobile service cards

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -9,17 +9,17 @@ export default function Services() {
   return (
     <>
       <div className="services-container">
-        <div className="brick-card">
+        <div className="brick-card" data-aos="fade-left">
           <img src={croppedBrickPath} alt="" />
           <h2>Brick</h2>
           <button className="free-quote-cta">Get Your Free Quote</button>
         </div>
-        <div className="block-card">
+        <div className="block-card" data-aos="fade-left">
           <img src={blockFoundation} alt="" />
           <h2>Block</h2>
            <button className="free-quote-cta">Get Your Free Quote</button>
         </div>
-        <div className="stone-card">
+        <div className="stone-card" data-aos="fade-left">
           <img src={fireplace} alt="" />
           <h2>Stone</h2>
            <button className="free-quote-cta">Get Your Free Quote</button>
