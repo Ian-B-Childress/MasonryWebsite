bb1d46b9ebe6ef83dddfb37ad37dab81ec3e5a1c
Ian Childress <howie2077@yahoo.com>
2025-08-20 18:16:15 -0400

    link to on services buttons

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -12,17 +12,23 @@ export default function Services() {
         <div className="brick-card" data-aos="fade-left">
           <img src={croppedBrickPath} alt="" />
           <h2>Brick</h2>
+          <Link to="/contact">
           <button className="free-quote-cta">Get Your Free Quote</button>
+          </Link>
         </div>
         <div className="block-card" data-aos="fade-left">
           <img src={blockFoundation} alt="" />
           <h2>Block</h2>
+          <Link to="/contact">
            <button className="free-quote-cta">Get Your Free Quote</button>
+           </Link>
         </div>
         <div className="stone-card" data-aos="fade-left">
           <img src={fireplace} alt="" />
           <h2>Stone</h2>
+          <Link to="/contact">
            <button className="free-quote-cta">Get Your Free Quote</button>
+           </Link>
         </div>
       </div>
     </>
