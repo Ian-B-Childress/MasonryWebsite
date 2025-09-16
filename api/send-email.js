d5aac541b1bf08cea50fbfc3fb401f6c96c07f4b
Ian Childress <howie2077@yahoo.com>
2025-09-16 19:13:25 -0400

    should work now

--- api/send-email.js
+++ api/send-email.js
@@ -1,4 +1,5 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
